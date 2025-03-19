15ac9da8fb8ee406befaf916f39523e56d41ab8d
Stemlc21 <Stefmalec4@gmail.com>
2025-03-19 17:44:13 +0200

    Finish 1 :)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
 import Profile from "./profile/Profile";
 import user from './user.json';
 import data from './data.json';
+import transactions from './transactions.json';
 import friends from './friends.json';
 import Statistics from "./statistics/Statistics";
 import FriendList from "./friendList/FriendList";
+import TransactionHistory from "./transactionHistory/TransactionHistory";
+
+
+
 
 
 function App() {
@@ -19,6 +24,7 @@ function App() {
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 }
